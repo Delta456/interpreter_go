1b60c68d0ab279ef83d5ce885d281569bf908ec9
Swastik <swstkbaranwal@gmail.com>
2024-06-12 00:49:43 +0530

    ast, parser: implement InfixExpr

--- ast/ast.go
+++ ast/ast.go
@@ -136,3 +136,22 @@ func (pe *PrefixExpression) String() string {
 
 	return out.String()
 }
+
+type InfixExpression struct {
+	Operator string // operator token like +
+	Left     Expression
+	Token    token.Token
+	Right    Expression
+}
+
+func (oe *InfixExpression) expressionNode()      {}
+func (oe *InfixExpression) TokenLiteral() string { return oe.Token.Literal }
+func (oe *InfixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(oe.Left.String())
+	out.WriteString(oe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
